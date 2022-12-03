a6426564681d2bdc7a98c2642ba93773ef7ef5cf
Fprog <fnaive@hotmail.com>
2022-12-02 21:47:27 -0300

    add dynamic header nav with JS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,65 @@
 import './index.css'
+
+let mobileNavInserted = false
+let desktopNavInserted = false
+
+const insertMobileNav = () => {
+  const $nav_wrapper = document.querySelector('.nav-wrapper')
+  const $mobile_nav = document.querySelector('#mobile-nav-template').content.cloneNode(true)
+  $nav_wrapper.appendChild($mobile_nav)
+  mobileNavInserted = true
+  if(desktopNavInserted) desktopNavInserted = false
+}
+
+const insertDesktopNav = () => {
+  const $nav_wrapper = document.querySelector('.nav-wrapper')
+  const $desktop_nav = document.querySelector('#desktop-nav-template').content.cloneNode(true)
+  $nav_wrapper.appendChild($desktop_nav)
+  desktopNavInserted = true
+  if(mobileNavInserted) mobileNavInserted = false
+}
+
+const useMobileNav = () => {
+  if(desktopNavInserted) {
+    const $desktopNavMenuContainer = document.querySelector('.nav-items-container')
+    $desktopNavMenuContainer.remove()
+  }
+  insertMobileNav()
+  return
+}
+
+const useDesktopNav = () => {
+  if(document.querySelector('.mobile-nav-menu-icon')) {
+    const $mobileNavMenu = document.querySelector('.mobile-nav-menu-icon')
+    $mobileNavMenu.remove()
+  }
+  insertDesktopNav()
+  return
+}
+
+const detectWidth = () => {
+  let mobileWidth = window.innerWidth >= 300 && window.innerWidth < 700 && !mobileNavInserted
+  let desktopWidth = window.innerWidth >= 700 && !desktopNavInserted
+  if(mobileWidth) {
+    useMobileNav()
+    return
+  } 
+  else if(desktopWidth) {
+    useDesktopNav()
+    return
+  }
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+  let mobileWidth = window.innerWidth >= 300 && window.innerWidth < 700
+  let desktopWidth = window.innerWidth >= 700
+  if(mobileWidth) {
+    insertMobileNav()
+  }
+  else if(desktopWidth) {
+    insertDesktopNav()
+  }
+  window.addEventListener('resize', () => {
+    detectWidth()
+  })
+})
