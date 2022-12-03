62b6f3867c50b453c365c8b40af4676c52cc198f
Fprog <fnaive@hotmail.com>
2022-12-03 12:28:33 -0300

    add uglify JS

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const TerserWebpackPlugin = require('terser-webpack-plugin')
 const path = require('path')
 
 module.exports = {
@@ -23,8 +24,10 @@ module.exports = {
     ]
   },
   optimization: {
+    minimize: true,
     minimizer: [
       new CssMinimizerPlugin(),
+      new TerserWebpackPlugin()
     ]
   },
   plugins: [
